03885ac863168b78bbbd397eaafd026ff7094c1b
jaiye-ola <oladisamuel@gmail.com>
2025-04-25 11:25:03 +0100

    another day at the office.

--- index.js
+++ index.js
@@ -1,61 +1,104 @@
-// DOM (Document Object Manipulation)
-// DOM in Javascript refers to your HTML file
+// More on objects
 
-
-let value;
-
-value = document; // document is the html file
-value = document.all[0];
-value = document.all[5];
-value = document.body;
-// value.style.backgroundColor="red";
-console.log(value);
-
-//traversing the DOM
-
-const header = document.getElementsByTagName("h1");
-const headerClass = document.getElementsByClassName("kelechi");
-const headerId = document.getElementById("timi");
-
-console.log(header);
-console.log(headerClass);
-console.log(headerId);
-
-const header1 = document.querySelector("h1");
-const header1Class = document.querySelector(".kelechi");
-const header1Id = document.querySelector("#timi");
-const list = document.querySelectorAll("li");
-
-
-
-
-list.forEach(items => {
-  items.style.backgroundColor = "blue";
-});
-
-const btn = document.querySelector("button");
-const input = document.querySelector(".inputItem")
-
-// btn.addEventListener("click", eventRunner);
-// btn.addEventListener("dblclick", eventRunner);
-// btn.addEventListener("mouseover", eventRunner);
-// btn.addEventListener("mouseout", eventRunner);
-// btn.addEventListener("mouseleave", eventRunner);
-// btn.addEventListener("mousemove", eventRunner);
-
-// input.addEventListener("keypress", eventRunner);
-// input.addEventListener("copy", eventRunner);
-// input.addEventListener("paste", eventRunner);
-// input.addEventListener("input", eventRunner);
-// input.addEventListener("change", eventRunner);
-// input.addEventListener("focus", eventRunner);
-input.addEventListener("blur", eventRunner);
-
-function eventRunner (e) {
-  e.preventDefault();
-  document.body.style.backgroundColor = `rgba($(e.offsetX), $(e.offsetY), 0)`;
-  console.log("clicked");
-  console.log(e);
-  console.log(e.target.value);
-  console.log(`Event, $(e.type)`);
+const person = {
+  name: "Oladipupo",
+  age: 78,
+  is_student: true,
+  greet: function (name) {
+    console.log(`Hello ${name}`)
+  }
 }
+
+console.log(person);
+person.greet("grace");
+
+
+// Factory functions - used to create multiple objects
+function createPerson (name, age, height, fav_dish) {
+  return {
+    name: name,
+    age: age,
+    height: height,
+    dish: fav_dish,
+    greet: () => {
+      console.log(`Hello ${name}`)
+    }
+  }
+}
+
+const person1 = createPerson("Divine", 34, "5'9", "Pounded Yam");
+person1.greet();
+console.log("Divine object", person1);
+
+
+
+// Constructor functions - used for promises
+
+
+function Person (firstName, lastName, age, dob, state) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.dob = dob;
+  this.state = state;
+  this.changeLastName = (newLastName) => {
+    this.lastName = newLastName;
+  }
+}
+
+const person2 = new Person("Grace", "Ogbonna", 545, "12-12-2012", "Ondo");
+const person3 = new Person("Tofunmi", "Gabriel", 545, "12-12-2012", "Ondo");
+person3.changeLastName("Sodiya");
+console.log("person 2", person2);
+console.log("person 3", person3);
+
+// Cloning objects
+const clone = { ...person };
+console.log("clone", clone);
+
+const assign = Object.assign({}, person)
+console.log(assign);
+
+const strings = new String("Hello World");
+const numbers = new  Number(12);
+console.log(typeof numbers);
+console.log(strings);
+
+// String objects
+const value = "Hello, world";
+const text = " kelechi";
+
+console.log(value.length);
+console.log(value.charAt(2));
+console.log(value.concat(text));
+console.log(value.indexOf("l"));
+console.log(value.lastIndexOf("l"));
+console.log(value.slice(0, 5));
+console.log(value.substring(0, 5));
+console.log(value.toLowerCase(0, 5));
+console.log(value.toUpperCase(0, 5));
+console.log(value.trim());
+console.log(value.split(','));
+console.log(value.replace("world", "emmanuel"));
+console.log(value.startsWith("w"));
+console.log(value.endsWith("d"));
+
+// Date objects
+const currentDate = new Date();
+console.log(currentDate.getFullYear());
+console.log(currentDate.getMonth() + 1);
+console.log(currentDate.getDate());
+console.log(currentDate.getMinutes());
+console.log(currentDate.getHours());
+console.log(currentDate.toLocaleDateString());
+console.log(currentDate);
+
+
+const setDate = new Date("2022-12-12");
+console.log(setDate.setFullYear("2026"));
+console.log(setDate.setMonth("3"));
+console.log(setDate.setDate("2"));
+console.log(setDate);
+
+// comparing date
+console.log(currentDate > setDate);
---
+++ keep5.js
@@ -0,0 +1,63 @@
+// DOM (Document Object Manipulation)
+// DOM in Javascript refers to your HTML file
+
+
+let value;
+
+value = document; // document is the html file
+value = document.all[0];
+value = document.all[5];
+value = document.body;
+// value.style.backgroundColor="red";
+console.log(value);
+
+//traversing the DOM
+
+const header = document.getElementsByTagName("h1");
+const headerClass = document.getElementsByClassName("kelechi");
+const headerId = document.getElementById("timi");
+
+console.log(header);
+console.log(headerClass);
+console.log(headerId);
+
+const header1 = document.querySelector("h1");
+const header1Class = document.querySelector(".kelechi");
+const header1Id = document.querySelector("#timi");
+const list = document.querySelectorAll("li");
+
+
+
+
+list.forEach(items => {
+  items.style.backgroundColor = "blue";
+});
+
+const btn = document.querySelector("button");
+const input = document.querySelector(".inputItem")
+
+// btn.addEventListener("click", eventRunner);
+// btn.addEventListener("dblclick", eventRunner);
+// btn.addEventListener("mouseover", eventRunner);
+// btn.addEventListener("mouseout", eventRunner);
+// btn.addEventListener("mouseleave", eventRunner);
+// btn.addEventListener("mousemove", eventRunner);
+
+// input.addEventListener("keypress", eventRunner);
+// input.addEventListener("copy", eventRunner);
+// input.addEventListener("paste", eventRunner);
+// input.addEventListener("input", eventRunner);
+// input.addEventListener("change", eventRunner);
+// input.addEventListener("focus", eventRunner);
+input.addEventListener("blur", eventRunner);
+
+function eventRunner (e) {
+  e.preventDefault();
+  document.body.style.backgroundColor = `rgba($(e.offsetX), $(e.offsetY), 0)`;
+  console.log("clicked");
+  console.log(e);
+  console.log(e.target.value);
+  console.log(`Event, $(e.type)`);
+}
+
+
